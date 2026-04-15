8d791316a188f494e0d089745026b19ae787eef9
HammazAbdelhadi <elhadihammaz@outlook.com>
2026-04-15 19:17:03 +0100

    larg video is 100Mb now

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -15,7 +15,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage: storage,
     fileFilter: fileFilter,
-    limits: { fileSize: 50 * 1024 * 1024 } // 50MB limit
+    limits: { fileSize: 100 * 1024 * 1024 } // 100MB limit
 });
 
 module.exports = upload;
